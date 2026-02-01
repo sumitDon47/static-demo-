328e8c055544d149199e862bfb395b43713f60a5
Sumit <sumit@pop-os.localdomain>
2026-02-01 11:00:08 +0545

    Hey you

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 
-noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * 200;
-  const y = Math.random() * 100;
-
+noBtn.addEventListener("mouseenter", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 120 - 60;
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
